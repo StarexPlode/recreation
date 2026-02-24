797b9c4ccfa708ee12b0620d1f98878f37a7b59e
StarexPlode <subhanyshaik@gmail.com>
2026-02-24 13:15:28 +0530

    Improved javascript, added button states and used used setTimeout method to change the button to submitted after 2s and back to idle after 3s

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
-const form = document.querySelector('#myForm')
-const button = document.querySelector('#sub')
+const form = document.querySelector("#myForm")
+const button = document.querySelector("#sub")
 
+let state = "idle";
+
+function RenderButton() {
+    if (state === "idle") {
+        button.textContent = "Submit";
+        button.disabled = !form.checkValidity();
+    }
+    if (state === "submitting") {
+        button.textContent = "Submitting...";
+        button.disabled = true;
+    }
+    if (state === "submitted") {
+        button.textContent = "Submitted!"
+        button.disabled = true;
+    }
+}
 form.addEventListener("input", function () {
-    button.disabled = !form.checkValidity();
+    if (state === "idle")
+    RenderButton();
 })
-button.addEventListener("click", function () {
-    button.textContent = "Submitting...";
-    button.disabled = true;
 
-    setTimeout(function () {
-        button.textContent = "Submitted!";
+button.addEventListener("click", function () {
+    if (state !== "idle") return;
+
+    state = "submitting";
+    RenderButton();
+
+    setTimeout(function() {
+        state = "submitted";
+        RenderButton();
+
+        setTimeout(function() {
+            state = "idle";
+            RenderButton();
+        }, 3000)
     }, 2000)
 })
