00b75b7e0bc20b01110d5ae1d0f53d64a913071a
StarexPlode <subhanyshaik@gmail.com>
2026-02-24 11:55:14 +0530

    added some js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const button = document.querySelector('#sub') 
+const form = document.querySelector('#myForm')
+const button = document.querySelector('#sub')
 
+form.addEventListener("input", function () {
+    button.disabled = !form.checkValidity();
+})
 button.addEventListener("click", function () {
     button.textContent = "Submitting...";
-    
+    button.disabled = true;
+
     setTimeout(function () {
         button.textContent = "Submitted!";
-        button.disabled = true;
-        console.dir(button)
     }, 2000)
 })
