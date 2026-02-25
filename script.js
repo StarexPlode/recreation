6fa95577674e199460ec8b489606299380b94e79
StarexPlode <subhanyshaik@gmail.com>
2026-02-25 21:02:52 +0530

    Removed the Theme toggle

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const form = document.querySelector("#myForm")
-const button = document.querySelector ("#sub")
+const button = document.querySelector("#sub")
 
 let state = "idle";
 
-function renderButton() {
+function buttonRender() {
     if (state === "idle") {
         button.textContent = "Submit";
         button.disabled = !form.checkValidity();
@@ -17,37 +17,22 @@ function renderButton() {
         button.disabled = true;
     }
 }
-
-form.addEventListener("input", function () {
-    renderButton();
+form.addEventListener("input", () => {
+    buttonRender()
 })
 
-button.addEventListener("click", function () {
+button.addEventListener("click", () => {
     
     state = "submitting";
-    renderButton();
+    buttonRender();
 
-    setTimeout(function () {
-        state = "submitted";
-        renderButton();
+    setTimeout(() => {
+        state = "submitted"
+        buttonRender();
 
-        setTimeout(function () {
+        setTimeout(() => {
             state = "idle";
-            renderButton();
+            buttonRender();
         }, 2000)
     }, 2000)
-})
-
-const theme = document.querySelector("#theme");
-const body = document.querySelector("#bady");
-
-function themeToggle() {
-    body.style.backgroundColor = "#888"
-    console.dir(body);
-}
-function themeToggle2 () {
-    body.classList.toggle("dark");
-}
-theme.addEventListener("click", function() {
-    themeToggle2()
 })
